1d264fd52433a360aa057d9999cafa3222aeb915
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-12-28 22:29:17 +0100

    Complete the DHT Processes

--- cap/iot/src/main/java/gos/integration/cap/iot/a2p/JsonToSqlProcessor.java
+++ cap/iot/src/main/java/gos/integration/cap/iot/a2p/JsonToSqlProcessor.java
@@ -5,12 +5,27 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
 public class JsonToSqlProcessor implements Processor {
+	static final String JMSTimestamp = "JMSTimestamp";
+	static final String NULL = "NULL";
+	static final String Dht11 = "dht11";
+	static final String TS = "ts";
+	static final String RH = "RH";
+	static final String AT = "t";
+	static final String T = "T";
+
+  private DateFormat sdf;
+	
+	public JsonToSqlProcessor() {
+		this.sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		this.sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 
 	@SuppressWarnings("unchecked")
 	public void process(Exchange exchange) throws Exception {
@@ -19,30 +34,43 @@ public class JsonToSqlProcessor implements Processor {
 		Message in = exchange.getIn();
 		Map<String, Object> map = (Map<String, Object>)(in.getBody(HashMap.class));
 		if (map != null) {
-			if(map.containsKey("ts")) {
-				tss = map.get("ts").toString();
+			if(map.containsKey(TS)) {
+				tss = map.get(TS).toString();
 			} else {
-				Long ts = in.getHeader("JMSTimestamp", -1L, Long.class);
-				DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+				Long ts = in.getHeader(JMSTimestamp, -1L, Long.class);
 				Date tsd = new Date(ts);
 				tss = sdf.format(tsd);
 			}
-			if(map.containsKey("t")) {
-				at = (Double)map.get("t");
+			if(map.containsKey(AT)) {
+				at = (Double)map.get(AT);
 			}
-			if(map.containsKey("dht11")) {
-				dht11 = (Double)map.get("dht11");
+			if(map.containsKey(Dht11)) {
+				dht11 = (Double)map.get(Dht11);
 			}
-			if(map.containsKey("RH")) {
-				rh = (Double)map.get("RH");
+			if(map.containsKey(RH)) {
+				rh = (Double)map.get(RH);
 			}
-			if(map.containsKey("T")) {
-				t = (Double)map.get("T");
+			if(map.containsKey(T)) {
+				t = (Double)map.get(T);
 			}
-			if(tss != null && at != null && dht11 != null && rh != null && t != null) {
-				String sql = String.format(
-						"INSERT INTO dht(ts, at, status, rh, t) VALUES ('%s', %f, %f, %f, %f)",
-						tss, at, dht11, rh, t);
+			
+			if(tss != null && at != null && dht11 != null) {
+				String values = String.format("'%s', %f, %f", tss, at, dht11);
+				StringBuilder sqlb = new StringBuilder(
+						"INSERT INTO dht(ts, at, status, rh, t) VALUES (" + values + ", ");
+				if(rh != null) {
+					sqlb.append(rh);
+				} else {
+					sqlb.append(NULL);
+				}
+				sqlb.append(", ");
+				if(t != null) {
+					sqlb.append(t);
+				} else {
+					sqlb.append(NULL);
+				}
+				sqlb.append(")");
+				String sql = sqlb.toString();
 				Message out = exchange.getOut();
 				out.setBody(sql);				
 			}
--- cap/iot/src/main/java/gos/integration/cap/iot/t2q/DhtProcessor.java
+++ cap/iot/src/main/java/gos/integration/cap/iot/t2q/DhtProcessor.java
@@ -2,33 +2,61 @@ package gos.integration.cap.iot.t2q;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
 public class DhtProcessor implements Processor {
+	static final String JMSTimestamp = "JMSTimestamp";
+	static final String TS = "ts";
+	static final String RH = "RH";
+	static final String T = "T";
+	
+	private DateFormat sdf;
+	
+	public DhtProcessor() {
+		this.sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		this.sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 
 	@SuppressWarnings("unchecked")
 	public void process(Exchange exchange) throws Exception {
 		Message in = exchange.getIn();
 		Map<String, Object> map = (Map<String, Object>)(in.getBody(HashMap.class));
-		
 		if (map != null) {
-			if(!map.containsKey("ts")) {
-				Long ts = in.getHeader("JMSTimestamp", -1L, Long.class);
-				DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+			if(!map.containsKey(TS)) {
+				Long ts = in.getHeader(JMSTimestamp, -1L, Long.class);
 				Date tsd = new Date(ts);
 				String tss = sdf.format(tsd);
-				map.put("ts", tss);
+				map.put(TS, tss);
+			}
+			if(map.containsKey(RH)) {
+				Double rh = (Double)map.get(RH);
+				if(rh != null) {
+					if(rh < 0.0 || rh > 100.0) {
+						map.remove(RH);
+					}
+				}
+			}
+			if(map.containsKey(T)) {
+				Double t = (Double)map.get(T);
+				if(t != null) {
+					if(t < -10.0 || t > 100.0) {
+						map.remove(T);
+					}
+				}
 			}
 		}
-		
 		Message out = exchange.getOut();
 		out.setBody(map);
 	}
-
 }
