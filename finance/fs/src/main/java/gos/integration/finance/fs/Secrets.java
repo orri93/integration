2c1e1a0a26003b04d67a037ec1458aa696b65364
Geirmundur Orri Sigurdsson <orri93@gmail.com>
2024-03-03 00:24:50 -0600

    Change the property source

--- finance/fs/src/main/java/gos/integration/finance/fs/Secrets.java
+++ finance/fs/src/main/java/gos/integration/finance/fs/Secrets.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:secrets.properties")
+@PropertySource(value = "classpath:secrets.properties")
 public class Secrets {
   @Value("${finance.database.url}")
   private String url;
